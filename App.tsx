6730fb0727f5a435b5d97532d39585ab280fc6cb
JMR193 <jmrsai987@gmail.com>
2025-11-02 04:50:52 -0800

    feat: Remove unused award showcase component
    The AwardShowcase component has been removed from the application. This component was previously used to display award-related imagery and information but is no longer needed as the visual presentation of the award has been consolidated into other sections or removed entirely. This change simplifies the main application structure and cleans up unused UI elements.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import Hero from './components/Hero';
 import GratitudeMessage from './components/GratitudeMessage';
-import AwardShowcase from './components/AwardShowcase';
 import WisdomGenerator from './components/WisdomGenerator';
 import SkillsLearned from './components/SkillsLearned';
 import Footer from './components/Footer';
@@ -12,7 +11,6 @@ const App: React.FC = () => {
   return (
     <div className="relative min-h-screen bg-gray-900 text-white overflow-x-hidden">
       <div className="absolute inset-0 bg-gradient-to-br from-indigo-900 via-purple-900 to-slate-900 opacity-80"></div>
-      <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/stardust.png')] opacity-20"></div>
       
       <Confetti />
 
@@ -20,7 +18,6 @@ const App: React.FC = () => {
         <div className="max-w-5xl mx-auto space-y-16 md:space-y-24">
           <Hero />
           <GratitudeMessage />
-          <AwardShowcase />
           <WisdomGenerator />
           <SkillsLearned />
           <Footer />
